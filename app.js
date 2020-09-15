bad174d27709f6c70df43bea421c691a9eb973af
gavin vaske <Gavin.Vaske@gmail.com>
2020-09-15 18:49:34 -0500

    Setup use of Cloud DB (mongoDB)

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
+
+if (env.error) {
+  console.log('Error loading .env');
+}
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+  .then(() => console.log('MongoDB connected...'))
+  .catch(error => console.log(error));
 
 const app = express();
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-
+ 
 const PORT = process.env.PORT || 3000;
 
 // Routes
