4245c4b774c072fedb1909645bd1f839639ae75d
gavin vaske <Gavin.Vaske@gmail.com>
2020-09-21 19:35:14 -0500

    Store registered users in database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
 
 router.get('/login', (request, response) => {
     response.render('login');
@@ -13,15 +16,12 @@ router.post('/register', (request, response) => {
   const {name, email, password, password2} = request.body;
   let errors = [];
 
-  // Check required fields
   if (!name || !email || !password || !password2) {
     errors.push({msg: 'Please fill in all fields'});
   }
-  // Check passwords match
   if (password != password2) {
     errors.push({msg: 'Passwords do not match'});
   }
-  // Check passwords length
   if (password.length < 6) {
     errors.push({msg: 'Password must be at least 6 characters'});
   }
@@ -29,10 +29,41 @@ router.post('/register', (request, response) => {
     response.render('register', {
       errors: errors,
       ...request.body
-
     });
   } else {
-    response.send('Pass');
+    User.findOne({email: email})
+      .then((user) => {
+        if (user) {
+          errors.push({msg: 'Account with that email already exists'});
+          response.render('register', {
+            errors: errors,
+            ...request.body
+          });
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          });
+          // Hash Password
+          bcrypt.genSalt(10, (error, salt) => {
+              bcrypt.hash(newUser.password, salt, (error, hash) => {
+                if (error) {
+                  throw error;
+                }
+                newUser.password = hash;
+                newUser.save()
+                  .then(user => {
+                    request.flash('success_msg', 'You are now registered!');
+                    response.redirect('/users/login');
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  });
+            });
+          });
+        }
+      });
   }
 });
 
