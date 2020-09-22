461cb44145b476ec5f99dbf74ae3ca86f8b22da3
gavin vaske <Gavin.Vaske@gmail.com>
2020-09-21 19:34:47 -0500

    Add middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const env = require('dotenv').config();
+const flash = require('connect-flash');
+const session = require('express-session');
 
 if (env.error) {
   console.log('Error loading .env');
@@ -20,7 +22,23 @@ app.set('view engine', 'ejs');
 
 // Body Parser
 app.use(express.urlencoded({extended: false}));
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Connect Flash
+app.use(flash());
  
+// Global Variables
+app.use((request, response, next) => {
+  response.locals.success_msg = request.flash('success_msg');
+  response.locals.error_msg = request.flash('error_msg');
+  next();
+});
 const PORT = process.env.PORT || 3000;
 
 // Routes
